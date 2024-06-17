c81dc1565652e3c84abf3922295c9fc0be181e24
Yves Zeng <yves.zeng@bytesforce.com>
2024-06-17 17:41:15 +0800

    job update

--- src/main/groovy/com/yixihan/yibot/config/BotConfig.groovy
+++ src/main/groovy/com/yixihan/yibot/config/BotConfig.groovy
@@ -1,23 +1,10 @@
 package com.yixihan.yibot.config
 
-import cn.hutool.core.collection.CollUtil
-import cn.hutool.core.thread.ThreadUtil
-import com.mikuac.shiro.core.Bot
-import com.mikuac.shiro.core.BotContainer
-import com.yixihan.yibot.utils.SystemUtils
 import groovy.util.logging.Slf4j
-import jakarta.annotation.Resource
 import lombok.Getter
 import org.springframework.beans.factory.InitializingBean
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
-
-import java.util.concurrent.CompletableFuture
-import java.util.concurrent.Future
 
 /**
  * description
---
+++ src/main/groovy/com/yixihan/yibot/model/JobParam.groovy
@@ -0,0 +1,20 @@
+package com.yixihan.yibot.model
+
+import com.mikuac.shiro.core.Bot
+
+/**
+ * description
+ *
+ * @author yixihan
+ * @date 2024-06-17 17:12
+ */
+class JobParam {
+
+    Bot bot
+
+    String jobCode
+
+    Date jobExecuteDate
+
+    String details
+}
