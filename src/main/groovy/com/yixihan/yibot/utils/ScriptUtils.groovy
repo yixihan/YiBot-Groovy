207ff4824848edefcef10e0ee349b946c9ffcac3
Yves Zeng <yves.zeng@bytesforce.com>
2024-06-25 10:04:08 +0800

    refactor(ScriptUtils): Refactoring Script Utils

--- src/main/groovy/com/yixihan/yibot/utils/ScriptUtils.groovy
+++ src/main/groovy/com/yixihan/yibot/utils/ScriptUtils.groovy
@@ -1,19 +1,18 @@
 package com.yixihan.yibot.utils
 
 import cn.hutool.core.io.FileUtil
-import cn.hutool.core.util.RandomUtil
+import cn.hutool.core.lang.Assert
+import cn.hutool.core.util.ObjUtil
 import cn.hutool.core.util.StrUtil
 import cn.hutool.system.SystemUtil
-import cn.hutool.system.oshi.OshiUtil
 import groovy.util.logging.Slf4j
 
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardCopyOption
-
 /**
- * description
+ * 脚本运行 工具类
  *
  * @author yixihan
  * @date 2024-06-17 09:31
@@ -22,23 +21,29 @@ import java.nio.file.StandardCopyOption
 class ScriptUtils {
 
     static String runPythonScript(String scriptPath, String... argument) {
+        // 读取脚本, 生成临时文件
+        Path tempPath = buildTempFile(scriptPath, "py")
+
+        // 构建Python命令
+        List<String> command = [SystemUtil.getOsInfo().isLinux() ? "python3" : "python"]
+
+        // 执行
+        return runScript(command, tempPath, argument)
+    }
+
+    static String runScript(List<String> command, Path scriptPath, String... argument) {
+        // 参数校验
+        Assert.notEmpty(command)
+        Assert.isTrue(ObjUtil.isNotNull(scriptPath) && FileUtil.exists(scriptPath, false))
+        return runScript(command, scriptPath.toFile(), argument)
+    }
+
+    static String runScript(List<String> command, File scriptFile, String... argument) {
+        Assert.notEmpty(command)
+        Assert.isTrue(ObjUtil.isNotNull(scriptFile) && FileUtil.exist(scriptFile))
         try {
-            if (!StrUtil.startWith(scriptPath, "/")) {
-                scriptPath = "/" + scriptPath
-            }
-            InputStream scriptData = ScriptUtils.class.getResourceAsStream(scriptPath)
-            if (scriptData == null) {
-                log.warn("script not exist")
-                return StrUtil.EMPTY
-            }
-
-            // 读取.py文件内容并写入临时文件
-            Path tempPath = Files.createTempFile("temp_script", ".py")
-            Files.copy(scriptData, tempPath, StandardCopyOption.REPLACE_EXISTING)
-
-            // 构建Python命令
-            String pyCommand = SystemUtil.getOsInfo().isLinux() ? "python3" : "python"
-            List<String> command = [pyCommand, tempPath.toFile().absolutePath]
+            // 构建命令
+            command.add(scriptFile.absolutePath)
             command.addAll(argument)
             log.info("command: [${command.join(" ")}]")
 
@@ -47,7 +52,7 @@ class ScriptUtils {
             processBuilder.redirectErrorStream(true)
             Process process = processBuilder.start()
 
-            // 读取Python脚本的输出
+            // 读取脚本的输出
             InputStream inputStream = process.getInputStream()
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
             StringBuilder output = new StringBuilder()
@@ -59,7 +64,7 @@ class ScriptUtils {
             // 等待进程结束并获取退出状态
             int exitCode = process.waitFor()
             // 清理临时文件
-            Files.delete(tempPath)
+            FileUtil.del(scriptFile)
 
             String outputStr = output.toString().trim()
             if (exitCode == 0) {
@@ -71,6 +76,31 @@ class ScriptUtils {
         } catch (Exception e) {
             log.error("Script Run Error : ${e.message}")
             return StrUtil.EMPTY
+        }
+    }
+
+    static Path buildTempFile(String scriptPath, String suffix) {
+        Path tempFile
+        try {
+            if (StrUtil.isBlank(scriptPath)) {
+                log.warn("scriptPath is Blank")
+                return null
+            }
+            scriptPath = !StrUtil.startWith(scriptPath, "/") ? "/" + scriptPath : scriptPath
+            InputStream scriptData = ScriptUtils.class.getResourceAsStream(scriptPath)
+            if (scriptData == null) {
+                log.warn("script not exist")
+                return null
+            }
+
+            // 读取文件内容并写入临时文件
+            suffix = !StrUtil.startWith(suffix, ".") ? "." + suffix : suffix
+            tempFile = Files.createTempFile("temp_script", suffix)
+            Files.copy(scriptData, tempFile, StandardCopyOption.REPLACE_EXISTING)
+            return tempFile
+        } catch (Exception e) {
+            log.error("Build Temp File Error, ${e.message}")
+            return null
         }
     }
 }
